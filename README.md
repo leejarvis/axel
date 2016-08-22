0809d7d41cdd7ff12afc4287cbbcc087d44702c5
Lee Jarvis <leejarvis@fastmail.com>
2016-08-22 14:35:38 +0100

    Add some basic documentation

--- README.md
+++ README.md
@@ -1,24 +1,4 @@
 # Axel
 
-**TODO: Add description**
-
-## Installation
-
-If [available in Hex](https://hex.pm/docs/publish), the package can be installed as:
-
-  1. Add `axel` to your list of dependencies in `mix.exs`:
-
-    ```elixir
-    def deps do
-      [{:axel, "~> 0.1.0"}]
-    end
-    ```
-
-  2. Ensure `axel` is started before your application:
-
-    ```elixir
-    def application do
-      [applications: [:axel]]
-    end
-    ```
-
+Axel is designed to provide a modular layer on top of an
+XML document. Search via XPath and map XML nodes to Elixir structs.
--- lib/axel.ex
+++ lib/axel.ex
@@ -4,6 +4,9 @@ defmodule Axel do
   Record.defrecord :xmlText,      Record.extract(:xmlText,      from_lib: "xmerl/include/xmerl.hrl")
   Record.defrecord :xmlAttribute, Record.extract(:xmlAttribute, from_lib: "xmerl/include/xmerl.hrl")
 
+  @doc """
+  Parses an XML string.
+  """
   def parse(xml) do
     {doc, []} = xml
     |> :binary.bin_to_list
@@ -11,10 +14,25 @@ defmodule Axel do
     doc
   end
 
+  @doc """
+  Returns a list of elements from an XPath search.
+
+  ## Examples
+
+    Axel.parse("<div>one</div><div>two</div>") |> Axel.search("//div")
+  """
   def search(node, xpath) do
     :xmerl_xpath.string(to_char_list(xpath), node)
   end
 
+  @doc """
+  Returns the first result in an XPath search. `nil` will be returned
+  if the search returned no results.
+
+  ## Examples
+
+    Axel.parse("<div>Hello</div>") |> Axel.find("//div")
+  """
   def find(node, xpath) do
     case search(node, xpath) do
       [h | _] -> h
@@ -22,6 +40,14 @@ defmodule Axel do
     end
   end
 
+  @doc """
+  Returns the text for this node.
+
+  ## Examples
+
+    iex> Axel.parse("<div>Hello</div>") |> Axel.text
+    "Hello"
+  """
   def text(node) do
     case find(node, "./text()") do
       xmlText(value: value) -> List.to_string(value)
@@ -29,6 +55,14 @@ defmodule Axel do
     end
   end
 
+  @doc """
+  Returns the attribute for this node, or `nil` if it does not exist.
+
+  ## Examples
+
+    iex> Axel.parse("<div id='123'></div>") |> Axel.attribute("id")
+    "123"
+  """
   def attribute(node, attr_name) do
     case find(node, "./@#{attr_name}") do
       xmlAttribute(value: value) -> List.to_string(value)
